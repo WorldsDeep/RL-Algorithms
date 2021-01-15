db021a5bc300fc4c7edb0febbb39b37254a77a69
WorldsDeep <76924121+WorldsDeep@users.noreply.github.com>
2021-01-15 12:44:47 -0700

    Update test_cartpole.py

--- REINFORCE/test_cartpole.py
+++ REINFORCE/test_cartpole.py
@@ -5,32 +5,52 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 from torch.distributions import Categorical
+import seaborn as sns
+sns.set(style='darkgrid')
 
-
-def main():
-    env = gym.make('CartPole-v1')
+def generate_history(lr, gamma,env,MAX_EPISODES):
+    
     in_dim=env.observation_space.shape[0]
     out_dim = env.action_space.n
     pi = Pi(in_dim, out_dim)
-    optimizer = optim.Adam(pi.parameters(), lr=0.01)
+    rew_history = []
+    
+    optimizer = optim.Adam(pi.parameters(), lr=lr)
 
-    for epi in range(400):
+    print('GAMMA=',gamma)
+    for epi in range(MAX_EPISODES):
         state = env.reset()
-        for t in range(200):
+        for t in range(300):
             action = pi.act(state)
             state, reward, done, _ = env.step(action)
             pi.rewards.append(reward)
             env.render()
             if done:
                 break
-        loss = train(pi, optimizer)
+        loss = train(pi, optimizer,gamma)
         total_reward = sum(pi.rewards)
-        solved = total_reward > 195.0
+        rew_history.append(total_reward)
+        solved = total_reward > 295.0
 
         pi.onpolicy_reset()
-        print(f'Episode {epi}, loss: {loss}, \
-        total_reward: {total_reward}, solved:{solved}')
-                   
+        print(f'Episode {epi},total_reward {total_reward}, solved:{solved}')
+    env.close()
+    print()
+    return rew_history
 
+def main():
+    env = gym.make('CartPole-v1')
+    
+    fig = plt.figure(figsize=(10,7))
+    MAX_EPISODES = 150
+    for g in [0.85,0.9,0.95,0.98]:
+        rew_history = generate_history(0.01,g,env,MAX_EPISODES)
+        sns.lineplot(x=[i for i in range(MAX_EPISODES)], y=rew_history,
+                     label='γ='+str(g))
+    plt.title('Total Reward History')
+    plt.xlabel('Episode')
+    plt.ylabel('Total Episode Reward')
+    
+    plt.show()
 if __name__ == '__main__':
     main()
